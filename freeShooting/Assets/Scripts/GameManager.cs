a27d157b9cf24abae880a4a9add5b72d6689a196
mohamedbahloul <53511257+mohamedbahloul@users.noreply.github.com>
2020-02-04 16:28:30 +0100

    fix

--- freeShooting/Assets/Scripts/GameManager.cs
+++ freeShooting/Assets/Scripts/GameManager.cs
@@ -1,4 +1,4 @@
-﻿ using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using System;
@@ -7,13 +7,26 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public Transform zone;
+    public TowerScript towerBase;
+    // public TowerScript[] towersselected = new TowerScript[6];
+
+
+
+    [SerializeField]
+
+    private GameObject itemparent;
+    //int i = 0;
+
+    private Transform TowerCanvas;
+    // Start is called before the first frame update
     public PlayerScript[] players;
     public GameObject inventory;
     public TowerScript[] Towers;
     public GameObject PlayerSlot;
     public GameObject PlayerSelection;
     private sceneManager sn;
-    private TowerScript[] towersSelected=new TowerScript[6];
+    private TowerScript[] towersSelected = new TowerScript[6];
 
     void Start()
     {
@@ -21,15 +34,35 @@ public class GameManager : MonoBehaviour
         sn = this.GetComponent<sceneManager>();
         remplirSelectedTower();
         Scene currentScene = SceneManager.GetActiveScene();
-        switch(currentScene.name)
+        switch (currentScene.name)
         {
             case "Game":
+                //getting the towers selected 
+                // towersSelected = this.GetComponent<GameManager>().GetSelectedTowers();
+                // Debug.Log(towersselected[0].name);
+                // getting the item parents to access in it to instatiate the towers that the player can build
+                TowerCanvas = GameObject.FindGameObjectWithTag("ItemsParent").transform;
+                //instantiate the tower base of the player 
+                zone = GameObject.FindGameObjectWithTag("TowerBaseZone").transform;
+                Instantiate(towerBase.prefab, zone);
+                // instantiate towers that the player can build
 
+
+                for (int i = 0; i < 6; i++)
+                {
+
+                    GameObject ChildGameObject1 = itemparent.transform.GetChild(i).gameObject;
+                    GameObject ChildGameObject2 = ChildGameObject1.transform.GetChild(0).gameObject;
+                    GameObject ChildGameObject3 = ChildGameObject2.transform.GetChild(0).gameObject;
+                    ChildGameObject3.GetComponent<Image>().sprite = towersSelected[i].image;
+
+
+                }
                 break;
             case "menu":
 
-             
-               
+
+
                 //instantiating the players to player selection menu 
                 playerInstantiate();
 
@@ -44,12 +77,12 @@ public class GameManager : MonoBehaviour
                 }
                 break;
         };
-        
+
 
     }
     void Update()
     {
-        
+
     }
     public TowerScript[] GetSelectedTowers()
     {
@@ -59,7 +92,7 @@ public class GameManager : MonoBehaviour
     {
         for (int i = 0; i < players.Length; i++)
         {
-            GameObject go = Instantiate(PlayerSlot, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+            GameObject go = Instantiate(PlayerSlot, PlayerSelection.transform.position, Quaternion.identity) as GameObject;
 
             go.transform.SetParent(PlayerSelection.transform);
             GameObject ChildGameObject1 = go.transform.GetChild(0).gameObject;
@@ -67,20 +100,21 @@ public class GameManager : MonoBehaviour
             ChildGameObject2.GetComponent<Image>().sprite = players[i].image;
             RegisterListener(ChildGameObject1, i);
         }
+     
     }
-    public void RegisterListener(GameObject obj,int i)
+    public void RegisterListener(GameObject obj, int i)
     {
         Button myButton = obj.GetComponent<Button>();
-        myButton.onClick.AddListener(() => { sn.OnPlayerClick(obj,i); });
-        
+        myButton.onClick.AddListener(() => { sn.OnPlayerClick(obj, i); });
+
     }
     public void remplirSelectedTower()
     {
         for (int j = 0; j < 6; j++)
         {
             towersSelected[j] = Towers[j];
-            
-            
+
+
         }
     }
 }
