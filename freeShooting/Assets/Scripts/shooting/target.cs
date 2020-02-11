36e4576a86bb1700cf98ac9bdeee7cf28a725fab
kh-akrimi <43071045+kh-akrimi@users.noreply.github.com>
2020-02-11 15:56:52 +0100

    fix mouvement, add health ber

--- freeShooting/Assets/Scripts/shooting/target.cs
+++ freeShooting/Assets/Scripts/shooting/target.cs
@@ -1,21 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class target : MonoBehaviour
 {
     public short health = 100;
+    private short maxHealth;
+    private Image healthBar;
+    public GameObject healthBarInstatiate;
+    public Vector3 healthOffset;
 
     private void Start()
     {
+        GameObject obj = Instantiate(healthBarInstatiate, transform.position+ healthOffset, transform.rotation,transform);
+        obj.transform.LookAt(Camera.main.transform);
+        healthBar = obj.transform.GetChild(0).GetChild(0).GetComponent<Image>();
+        maxHealth = health;
     }
     public void takeDamage(short damage)
     {
         health -= damage;
+        
         if (health <= 0)
         {
             die();
+
         }
+        healthBar.fillAmount = (float)health / (float)maxHealth;
     }
     void die()
     {
