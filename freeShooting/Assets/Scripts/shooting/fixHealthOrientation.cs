98cda171f19d6b6076a770d6614ad056aadaca6e
kh-akrimi <43071045+kh-akrimi@users.noreply.github.com>
2020-02-12 14:35:12 +0100

    add healthBar

---
+++ freeShooting/Assets/Scripts/shooting/fixHealthOrientation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fixHealthOrientation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (transform.position.z > 0)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.x, 0, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.x, 180f, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
