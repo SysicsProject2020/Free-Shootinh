b529c3e4b1bbc7c00944008301065ef4c17ff988
mohamedbahloul <53511257+mohamedbahloul@users.noreply.github.com>
2020-02-15 16:03:05 +0100

    player movement with slide bar

--- freeShooting/Assets/Scripts/manager/GameManagerPartie.cs
+++ freeShooting/Assets/Scripts/manager/GameManagerPartie.cs
@@ -5,18 +5,20 @@ using UnityEngine.UI;
 
 public class GameManagerPartie : MonoBehaviour
 {
+    
     public TowerScript towerBase;
     public TowerScript enemybase;
     public PlayerScript player;
     public PlayerScript enemy;
     public GameObject itemParent;
-    private Vector3 playerPos = new Vector3(5, 1.8f, -25);
+    public static  Vector3 playerPos = new Vector3(5, 1.8f, -25);
     private Vector3 enemypos = new Vector3(5, 1.8f, 25);
     private Vector3 playerTowerPos = new Vector3(5, 2.2f, -38);
     private Vector3 enemyTowerPos = new Vector3(5, 2.2f, 38);
     private TowerScript[] towersSelected = new TowerScript[6];
     private TowerScript[] EnemySelectedTowers = new TowerScript[6];
 
+    public static GameObject playerPrefab;
     public static GameObject enemy_;
     void Start()
     {
@@ -34,7 +36,7 @@ public class GameManagerPartie : MonoBehaviour
         Instantiate(towerBase.prefab, playerTowerPos, Quaternion.Euler(0, 0, 0));
         Instantiate(enemybase.prefab, enemyTowerPos, Quaternion.Euler(-180, 0, 0));
 
-        Instantiate(player.prefab, playerPos, Quaternion.Euler(0, 0, 0));
+        playerPrefab= Instantiate(player.prefab, playerPos, Quaternion.Euler(0, 0, 0));
         enemy_ = Instantiate(enemy.prefab, enemypos, Quaternion.Euler(-180, 0, 0));
     }
     private void ChangeSprites()
@@ -48,4 +50,12 @@ public class GameManagerPartie : MonoBehaviour
     {
         EnemySelectedTowers = AIeasy.selectedcards;
     }
+   /* public void MovePlayer(float position)
+    {
+        Vector3 pos = player.prefab.transform.position;
+        pos.x = position * 5;
+        player.prefab.transform.position = pos;
+        Debug.Log("fdsf");
+
+    }*/
 }
