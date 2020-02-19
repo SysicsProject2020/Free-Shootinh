6d0452f51ed214f5a965e7e8890e13b4e32a3eec
mohamedbahloul <53511257+mohamedbahloul@users.noreply.github.com>
2020-02-19 13:26:35 +0100

    nothing

--- freeShooting/Assets/Scripts/PlayerMovementWithSliderBar.cs
+++ freeShooting/Assets/Scripts/PlayerMovementWithSliderBar.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PlayerMovementWithSliderBar : MonoBehaviour
 {
+    public float speed =10f;
      public void MovePlayer(float position)
     {
+        Vector3 pos = new Vector3(position, GameManagerPartie.playerPos.y, GameManagerPartie.playerPos.z);
+        //GameManagerPartie.player_.transform.position = Vector3.Lerp(GameManagerPartie.player_.transform.position, pos, speed * Time.deltaTime);
         GameManagerPartie.player_.transform.position = new Vector3(position, GameManagerPartie.playerPos.y, GameManagerPartie.playerPos.z);
         Debug.Log(GameManagerPartie.player_.transform.position);
     }
