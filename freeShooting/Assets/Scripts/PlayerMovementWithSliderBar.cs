01f09653e9a31a95822c98dddd587a6c511615de
mohamedbahloul <53511257+mohamedbahloul@users.noreply.github.com>
2020-02-19 12:28:34 +0100

    player slider

---
+++ freeShooting/Assets/Scripts/PlayerMovementWithSliderBar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementWithSliderBar : MonoBehaviour
+{
+     public void MovePlayer(float position)
+    {
+        GameManagerPartie.player_.transform.position = new Vector3(position, GameManagerPartie.playerPos.y, GameManagerPartie.playerPos.z);
+        Debug.Log(GameManagerPartie.player_.transform.position);
+    }
+}
