f420ca1d43b2e15c236f8301d2010463a138fd4f
mohamedbahloul <53511257+mohamedbahloul@users.noreply.github.com>
2020-02-28 15:35:58 +0100

    start magic

---
+++ freeShooting/Assets/Scripts/MagicFunctions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void destroyAll()
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            for(int j = 0; j < 2; j++)
+            {
+                if (positionManager.buildingGameObject[i,j] != null)
+                {
+                    Destroy(positionManager.buildingGameObject[i, j].gameObject);
+                    positionManager.buildingGameObject[i, j] = null;
+                    positionManager.buildingTowerScript[i, j] = null;
+                }
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
