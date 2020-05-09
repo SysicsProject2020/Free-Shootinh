3b249ba9fae720d4f70086e69f6b25477b7c595a
mohamedbahloul <53511257+mohamedbahloul@users.noreply.github.com>
2020-05-09 17:28:05 +0100

    loading system

---
+++ freeShooting/Assets/Scripts/loadingLevel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using TMPro;
+
+public class loadingLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject loadingScreen;
+    public Slider slider;
+    public TextMeshProUGUI ProgressTxt;
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+            Debug.Log(progress);
+            ProgressTxt.text = progress * 100f + "%";
+            yield return null;
+        }
+    }
+}
