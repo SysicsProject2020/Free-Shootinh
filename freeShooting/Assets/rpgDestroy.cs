80dea8ef7d0473be2a733b27d533c26473e57ae4
kh-akrimi <43071045+kh-akrimi@users.noreply.github.com>
2020-05-17 16:16:30 +0100

    fixes guns

---
+++ freeShooting/Assets/rpgDestroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rpgDestroy : MonoBehaviour
+{
+    public float timeDestroy = 0.7f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DestroyEffect());
+    }
+
+    IEnumerator DestroyEffect()
+    {
+        yield return new WaitForSeconds(timeDestroy);
+        Destroy(gameObject);
+    }
+}
