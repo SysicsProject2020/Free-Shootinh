edd73b53c72de3e662399e32a8afbe4312894e4c
mohamedbahloul <53511257+mohamedbahloul@users.noreply.github.com>
2020-06-15 02:13:41 +0100

    Ads

---
+++ freeShooting/Assets/Scripts/AdsManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour, IUnityAdsListener
+{
+    string gameId = "1234567";
+    string myPlacementId = "rewardedVideo";
+    bool testMode = true;
+
+    // Initialize the Ads listener and service:
+    void Start()
+    {
+      
+    }
+    public void showAd()
+    {
+        Advertisement.AddListener(this);
+        Advertisement.Initialize(gameId, testMode);
+
+    }
+    // Implement IUnityAdsListener interface methods:
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        // Define conditional logic for each ad completion status:
+        if (showResult == ShowResult.Finished)
+        {
+            GameManager.instance.diamond += 1000;
+            SaveSystem.SavePlayer();
+            Advertisement.RemoveListener(this);
+        }
+        else if (showResult == ShowResult.Skipped)
+        {
+            // Do not reward the user for skipping the ad.
+        }
+        else if (showResult == ShowResult.Failed)
+        {
+            Debug.LogWarning("The ad did not finish due to an error.");
+        }
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+
+        // If the ready Placement is rewarded, show the ad:
+        if (placementId == myPlacementId)
+        {
+            
+         
+            Advertisement.Show(myPlacementId);
+        }
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+        // Log the error.
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        // Optional actions to take when the end-users triggers an ad.
+    }
+}
